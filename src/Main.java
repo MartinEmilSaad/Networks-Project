9b3755e608de34107b7584a0c8f389e601b2d608
MartinEmilSaad <martin_emil345@yahoo.com>
2018-11-22 02:43:47 +0200

    argument to the main added

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
 
+        if(args.length>=1){
+         //   System.out.println(args[0]);
+            String default_path2 = new String(System.getProperty("user.dir"));
+            File file22 = new File(default_path2+"/hey.txt");
+            try {
+                boolean c = file22.createNewFile();
+                try( FileWriter fw = new FileWriter(file22) ){
+                    fw.write(args[0]);
+                }
+                catch (Exception e){
+                    System.out.println("Error : " + e);
+                }
+
+            }
+            catch (Exception u){
+                System.out.println("error");
+            }
+        }
+
+
 
         String stringData = new String();
         String stringGenerator = new String();
+        String path_with_colons = new String("");
         String path = new String();
 
         Scanner get_input = new Scanner(System.in);
@@ -17,13 +38,19 @@ public class Main {
         File output1 ;
         File output2 ;
 
-        System.out.println("Inputs : ");
+        /*
+
+      //  System.out.println("Inputs : ");
         //get path to file
-        path = get_input.next() ;
+        path_with_colons = get_input.nextLine() ;
+        int index1 = path_with_colons.indexOf("");
+        int index2 = path_with_colons.indexOf('"', index1+2);
+        path = path_with_colons.substring(index1+1 , index2) ;
+        */
         try
             {
                 String default_path = new String(System.getProperty("user.dir"));
-                file = new File(default_path+"/"+path);
+                file = new File(default_path+"/"+args[0]);
                 //open file
                 Scanner read_from_file = new Scanner(file);
                 int i = 0 ;
